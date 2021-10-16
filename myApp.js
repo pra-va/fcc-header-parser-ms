ecb168442ef4c4e228a7c706088389cf2e353fa1
Pranas Varaneckas <varaneckas@gmail.com>
2021-10-16 13:12:11 +0300

    forgot to commit myApp.js

---
+++ myApp.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router
+    .get("/whoami", (req, res) => {
+        res.json({
+            ipaddress: req.ip,
+            language: req.headers["accept-language"],
+            software: req.headers["user-agent"]
+        })
+    });
+
+module.exports = router;
